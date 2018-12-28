020aaa7b1f5a79288a1aee42c7ecdec0910a06fc
cambiph <philippe.cambien@gmail.com>
2018-12-28 11:07:10 +0100

    Bugfix

--- spec.js
+++ spec.js
@@ -1,13 +1,13 @@
 describe('Actions API test case', function() {
 
     it('moveto does not get recognized as a command', function() {
-        browser.get(baseUrl).then(function() {
+        browser.get(browser.baseUrl).then(function() {
             browser.actions().mouseMove(element(by.css('.downloadBox'))).perform();
         });
     });
 
     it('moveto does not get recognized as a command using legacy mode', function() {
-        browser.get(baseUrl).then(function() {
+        browser.get(browser.baseUrl).then(function() {
             browser.actions({bridge: true}).mouseMove(element(by.css('.downloadBox'))).perform();
         });
     });
