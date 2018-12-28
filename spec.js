c7517dbde6fc3c482e57b746ac5c07b6088f0a3d
cambiph <philippe.cambien@gmail.com>
2018-12-28 11:04:27 +0100

    Fix url

--- spec.js
+++ spec.js
@@ -1,13 +1,13 @@
 describe('Actions API test case', function() {
 
     it('moveto does not get recognized as a command', function() {
-        browser.get('http://www.seleniumhq.org').then(function() {
+        browser.get('www.seleniumhq.org').then(function() {
             browser.actions().mouseMove(element(by.css('.downloadBox'))).perform();
         });
     });
 
     it('moveto does not get recognized as a command using legacy mode', function() {
-        browser.get('http://www.seleniumhq.org').then(function() {
+        browser.get('www.seleniumhq.org').then(function() {
             browser.actions({bridge: true}).mouseMove(element(by.css('.downloadBox'))).perform();
         });
     });
